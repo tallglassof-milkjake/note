f5fce722a9094c3d9239552c111c1e593e9030bf
Jake Mackinlay <jrmackinlay90@gmail.com>
2020-11-03 19:24:06 +1030

    reading data from db.json and sending to /api/notes correctly

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const path = require("path");
-
+const fs = require("fs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+// Send HTML to browser
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
@@ -18,9 +18,28 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+// Send data to browser
+const tastyNotes = fs.readFile("db/db.json", "utf8", function(err, data) {
+    if (err) throw err;
+    
+    app.get("/api/notes", function(req, res) {
+        res.json(data);
+    });
+
+    console.log(data);
+});
 
 
+// Recieve data from browser
+app.post("/api/notes", function(req, res) {
+    tastyNotes.push(req.body);
+    res.json(true);
+});
 
+// Delete notes
+// app.delete("/api/notes/:id", function(req, res) {
+
+// })
 
 
 app.listen(PORT, function() {
